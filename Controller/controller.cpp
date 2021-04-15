074d6db0394aa57f45409a455a371bbf80256a95
Roman Matsa <matsars@gmail.com>
2021-04-15 11:19:37 +0300

    make Controller class constructor

--- Controller/controller.cpp
+++ Controller/controller.cpp
@@ -1,6 +1,10 @@
 #include "controller.h"
 
-Controller::Controller(QObject *parent) : QObject(parent)
+Controller::Controller(QImage *inputImage, QImage *outputImage, QString conversion, QObject *parent)
+    : QObject(parent)
+    , inputImage(inputImage)
+    , outputImage(outputImage)
+    , conversion(conversion)
 {
 
 }
--- Controller/controller.h
+++ Controller/controller.h
@@ -8,7 +8,12 @@ class Controller : QObject
     Q_OBJECT
 
 public:
-    Controller(QObject *parentt = nullptr);
+    Controller(QImage *inputImage, QImage *outputImage, QString conversion, QObject *parent = nullptr);
+private:
+    QImage *inputImage {nullptr};
+    QImage *outputImage {nullptr};
+
+    QString conversion;
 };
 
 #endif // CONTROLLER_H
